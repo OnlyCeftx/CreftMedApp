3f34df09aa1c4ad4566fcabbe499cc2f29006e2f
ceftx <claudiocmorab@gmail.com>
2024-12-16 11:23:33 -0400

    Update

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -4,14 +4,6 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import axios from "axios";
-
-// Configura Axios para incluir el token CSRF en los encabezados
-axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
-
-axios.defaults.headers.common["X-CSRF-TOKEN"] = document
-    .querySelector('meta[name="csrf-token"]')
-    .getAttribute("content");
 
 import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,11 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title inertia>{{ config('app.name', 'Laravel') }}</title>
-
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
